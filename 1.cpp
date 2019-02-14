d47982f0c7f59cd9b077e6e6be723415abe80fbd
Nehasingh1300 <40912375+Nehasingh1300@users.noreply.github.com>
2019-02-14 10:57:09 +0530

    Update 1.cpp
    Comments has been deleted

--- 1.cpp
+++ 1.cpp
@@ -5,34 +5,33 @@ using namespace std;
   
 int main() 
 { 
-  
-    // This pointer will hold the base address of the block created 
+ 
     int* ptr; 
     int n, i, sum = 0; 
   
-    // Get the number of elements for the array 
+    
     
     cout<<"Enter number of elements: "; 
     cin>>n;
 
-    // Dynamically allocate memory using malloc() 
+    
     ptr = (int*)calloc(n, sizeof(int)); 
   
-    // Check if the memory has been successfully allocated by malloc or not 
+    
     if (ptr == NULL) 
     { 
        	cout<<"Memory not allocated.\n"; 
         exit(0); 
     } 
     else { 
- 	// Get the elements of the array
+ 	
 	cout<<"Enter the elements ";
 	for(i = 0; i < n; ++i)
 	{
 		cin>>ptr[i];
 	}
   
-        // Print the elements of the array 
+        
         cout<<"\n\nThe elements of the array are: \n"; 
         for (i = 0; i < n; ++i) 
 	{ 
